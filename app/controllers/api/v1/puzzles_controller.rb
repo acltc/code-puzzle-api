e88dafa05e70465e8bc8e16d592d34033ac02c3e
Chelsea <chelsea.dommert@gmail.com>
2014-07-27 10:42:33 -0500

    Adds 'create' action to api

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::PuzzlesController < ApplicationController
+
+  def create
+    @puzzle = Puzzle.new(puzzle_params)
+    @puzzle.save
+  end
+
+
+  private
+
+  def puzzle_params
+    return params.require(:puzzle).permit(:name, :instructions, :solution)
+  end
+
 end
---
+++ app/views/api/v1/puzzles/_puzzle.json.jbuilder
@@ -0,0 +1,6 @@
+json.id               puzzle.id
+json.name             puzzle.name
+json.instructions     puzzle.instructions
+json.solution         puzzle.solution
+json.created_at       puzzle.created_at
+json.updated_at       puzzle.updated_at
