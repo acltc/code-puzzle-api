c8afca8946c5993db96b7d58a0634ec8cd9a2dfc
Yedidya Weiner <ydweiner17@gmail.com>
2014-07-27 10:50:18 -0500

    fixed one test

--- spec/requests/api/v1/puzzles_spec.rb
+++ spec/requests/api/v1/puzzles_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'GET /api/v1/puzzles/:id' do
 
   it 'should return a puzzle by its id' do
-    employee = Puzzle.create(name: "Multiples of 3 and 5", instructions: "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.", solution: "nil")
+    puzzle = Puzzle.create(name: "Multiples of 3 and 5", instructions: "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.", solution: "nil")
 
     get "/api/v1/puzzles/#{puzzle.id}.json"
 
