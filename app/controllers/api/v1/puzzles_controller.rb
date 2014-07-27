ac8a5a63a1e857d56d2c849d6a03121b8f6d3923
Chelsea <chelsea.dommert@gmail.com>
2014-07-27 11:11:41 -0500

    Gets the following tests to pass: instructions presence validation, name uniqueness validation

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -6,12 +6,16 @@ class Api::V1::PuzzlesController < ApplicationController
 
   def create
     @puzzle = Puzzle.new(puzzle_params)
-    @puzzle.save
+    if @puzzle.save
+    else
+      render :json => @puzzle.errors
+    end
   end
 
   private
 
   def puzzle_params
     return params.require(:puzzle).permit(:name, :instructions, :solution)
+  end
     
 end
--- spec/requests/puzzles_spec.rb
+++ spec/requests/puzzles_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'POST /api/v1/puzzles' do
+  describe 'POST /api/v1/puzzles' do
 
     it 'should create new puzzle and save name, instructions, solution, and created_at' do
       
@@ -37,14 +37,29 @@ describe 'POST /api/v1/puzzles' do
           name: "Sample Arithmetic Puzzle",
           instructions: "",
           solution: "1 + 1",
-          created_at: "Now"
         }
       }.to_json, {'Content-Type' => 'application/json'}
 
       expect(response_json).to eq({
-        "message" => "Puzzle not created",
-        "errors" => ["Instructions can't be blank"]
+        "instructions" => ["can't be blank"]
       })
     end
+
+    it 'should not save two puzzles w same name' do
+      Puzzle.create({name: "test", instructions:"abc", solution: "xyz"})
+
+      post 'api/v1/puzzles.json', {
+        puzzle: {
+          name: "test",
+          instructions: "Write code that returns the sum of 1 and 1",
+          solution: "1 + 1",
+        }
+      }.to_json, {'Content-Type' => 'application/json'}
+
+      expect(response_json).to eq({
+        "name" => ["has already been taken"]
+      })
+    end
+
   end
 
